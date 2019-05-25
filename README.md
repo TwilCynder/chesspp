7626fe8da181673099016c47bff54a667e9ac909
TwilCynder <twilcynder@gmail.com>
2019-05-26 00:40:49 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # chesspp
 Chess++, or the Evlolution of Chess (ok maybe not)
+
+A turn-based strategy game based on Chess.
+
+STANDALONE VERSION
---
+++ main.js
@@ -0,0 +1,13 @@
+//CHESS++ PROJECT
+//By TwilCynder
+
+
+//lancée par p5 au début 
+function setup(){
+
+}
+
+//non jdéconne jamais j'utilise p5
+function main(){
+    
+}
